430a077d342f5b9f9122950f1568919f520c74ca
Lloyd Kayembe <kayembeloyd@gmail.com>
2021-01-27 08:21:20 +0200

    BEAUTIFY PHASE 1 - Basic app card (Changed the color of the progress bar coz it wasnt looking good on the real app) - Expandable app card - Solve the issue concerning the background of the text input layout (NOT WORKED) - Work on the all aps indicator - Work on the search bar for all apps (NOT WORKED)

--- app/src/main/java/com/loycompany/deepee/adapters/MainAppCardRecyclerViewAdapter.java
+++ app/src/main/java/com/loycompany/deepee/adapters/MainAppCardRecyclerViewAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +14,8 @@ import com.loycompany.deepee.R;
 import com.loycompany.deepee.classes.AppCard;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainAppCardRecyclerViewAdapter extends RecyclerView.Adapter<MainAppCardRecyclerViewAdapter.ViewHolder> {
 
@@ -38,8 +42,41 @@ public class MainAppCardRecyclerViewAdapter extends RecyclerView.Adapter<MainApp
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView appIconCircleImageView;
+        TextView appNameTextView;
+        LinearLayout linearLayoutExpandable;
         public ViewHolder(View itemView) {
             super(itemView);
+
+            appIconCircleImageView = itemView.findViewById(R.id.app_icon_circle_image_view);
+            appNameTextView = itemView.findViewById(R.id.app_name_textView);
+            linearLayoutExpandable = itemView.findViewById(R.id.linear_layout_expandable);
+
+            appIconCircleImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (linearLayoutExpandable.getVisibility() == View.GONE){
+                        linearLayoutExpandable.setVisibility(View.VISIBLE);
+                        appNameTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_up_24, 0);
+                    } else{
+                        linearLayoutExpandable.setVisibility(View.GONE);
+                        appNameTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_down_24, 0);
+                    }
+                }
+            });
+
+            appNameTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (linearLayoutExpandable.getVisibility() == View.GONE){
+                        linearLayoutExpandable.setVisibility(View.VISIBLE);
+                        appNameTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_up_24, 0);
+                    } else{
+                        linearLayoutExpandable.setVisibility(View.GONE);
+                        appNameTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_down_24, 0);
+                    }
+                }
+            });
         }
     }
 
