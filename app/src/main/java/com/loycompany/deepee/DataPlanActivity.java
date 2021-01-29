8e5c68119f0627078882aa5d81d0cf205e997d3a
Lloyd Kayembe <kayembeloyd@gmail.com>
2021-01-29 12:30:28 +0200

    Work on the pie chart view

--- app/src/main/java/com/loycompany/deepee/DataPlanActivity.java
+++ app/src/main/java/com/loycompany/deepee/DataPlanActivity.java
@@ -2,13 +2,25 @@ package com.loycompany.deepee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
 public class DataPlanActivity extends AppCompatActivity {
 
     Spinner dataTypeSpinner;
+
+    PieChartView pieChartView;
+    List<SliceValue> pieData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +34,15 @@ public class DataPlanActivity extends AppCompatActivity {
 
         dataTypeSpinner.setAdapter(arrayAdapter);
         dataTypeSpinner.setSelection(0);
+
+        pieChartView = findViewById(R.id.pie_chart_view);
+
+        pieData = new ArrayList<>();
+
+        pieData.add(new SliceValue(15, Color.BLUE));
+        pieData.add(new SliceValue(45, Color.GREEN));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartView.setPieChartData(pieChartData);
     }
 }
