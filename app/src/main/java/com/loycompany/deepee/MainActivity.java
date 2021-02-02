52c5ac6b129fd9cf9e75a55fcac5b7c9663e48d3
Lloyd Kayembe <kayembeloyd@gmail.com>
2021-02-02 16:11:02 +0200

    When a back button has been pressed it should first go to the activeDp tab

--- app/src/main/java/com/loycompany/deepee/MainActivity.java
+++ app/src/main/java/com/loycompany/deepee/MainActivity.java
@@ -3,9 +3,11 @@ package com.loycompany.deepee;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.loycompany.deepee.adapters.MainViewPagerAdapter;
@@ -31,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
         mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(mainViewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /**
+     * Called when the activity has detected the user's press of the back
+     * key. The {@link #getOnBackPressedDispatcher() OnBackPressedDispatcher} will be given a
+     * chance to handle the back button before the default behavior of
+     * {@link Activity#onBackPressed()} is invoked.
+     *
+     * @see #getOnBackPressedDispatcher()
+     */
+    @Override
+    public void onBackPressed() {
+        if (tabLayout.getSelectedTabPosition() == 2 || tabLayout.getSelectedTabPosition() == 1){
+            viewPager.setCurrentItem(0);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
